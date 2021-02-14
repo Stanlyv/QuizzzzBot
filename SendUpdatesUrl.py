fa61a240c322e401eb2cac43605e048cb428a6eb
Stanlyv <satana2026@gmail.com>
2021-02-14 16:51:28 +0200

    Добавлена возможность добавлять человеческий текст

--- SendUpdatesUrl.py
+++ SendUpdatesUrl.py
@@ -2,7 +2,15 @@ import urllib.request
 import config
 
 chat_ids = [199610041, 273077469, 308729527, 884721281]
-text = """Game%20Quiz%20Bot%20v0.3%20has%20been%20released!%0AChangelog:%0A1.%20Categories%20added%0A2.%20Buttons%20for%20categories%20added%0A3.%20Bugs%20for%20buttons%20for%20categories%20added%0A4.%20DB%20reworked%0A5.%20Moi%20nervi%20ne%20vuderjivaut"""
+text = """Game Quiz Bot v0.3 has been released!
+Changelog:
+1. Categories added
+2. Buttons for categories added
+3. Bugs for buttons for categories added
+4. DB reworked
+5. Moi nervi ne vuderjivaut"""
+text = text.replace(" ", "%20")
+text = text.replace("\n", "%0A")
 
 for i in chat_ids:
     urllib.request.urlopen(f"https://api.telegram.org/bot{config.Token}/sendMessage?chat_id={i}&text={text}")
